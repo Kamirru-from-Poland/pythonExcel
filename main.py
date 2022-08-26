3e32e471bdb48da831759009fc97663a9cddae8c
Kamil Kordos <97972540+Kamirru-from-Poland@users.noreply.github.com>
2022-08-26 13:18:56 +0200

    first steps

--- main.py
+++ main.py
@@ -7,4 +7,16 @@ print(sheet)
 print(type(sheet))
 print(sheet.title)
 print(wb.active)
+print("-------")
+print(sheet['A1'])
+print(sheet['A1'].value)
+cell = sheet['B2']
+print(cell.value)
+print("Row " + str(cell.row) + ", column " + str(cell.column) + " has value " + str(cell.value) + ".")
+print("Cell " + str(cell.coordinate) + ' has value ' + str(cell.value) + '.')
+print("-------")
+print(sheet.cell(row=1, column=2).value)
+for i in range(1, 8, 2):
+    print(i, sheet.cell(row=i, column=2).value)
+
 
