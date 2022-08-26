dfdf0a450508d5fda2311989acd02f9b210bb241
Kamil Kordos <97972540+Kamirru-from-Poland@users.noreply.github.com>
2022-08-26 12:57:07 +0200

    initial

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print(2+3)
+import openpyxl
+wb = openpyxl.load_workbook('file.xlsx')
+print(type(wb))
+print(wb.sheetnames)
+sheet = wb['S1']
+print(sheet)
+print(type(sheet))
+print(sheet.title)
+print(wb.active)
+
